c11b0bc0b0b7f9e568bd5771a7b9e533f33f8951
DylanTuna <dylant@Perion.com>
2021-02-01 19:05:44 +0100

    Setup vue3-google-oauth2 library

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,18 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './index.css'
+//@ts-ignore
+import GAuth from 'vue3-google-oauth2'
 
-createApp(App)
+const gauthOption = {
+  clientId: import.meta.env.VITE_GOOGLE_CLIENT_ID,
+  scope: 'profile email',
+  prompt: 'select_account',
+  // fetch_basic_profile: false
+}
+const app = createApp(App)
 .use(router)
-.mount('#app')
+
+app.use(GAuth, gauthOption)
+
+app.mount('#app')
